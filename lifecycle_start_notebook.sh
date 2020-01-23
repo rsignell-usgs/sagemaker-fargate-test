79e17f086302b75e80764afe801593b3c86ef883
Rich Signell <rsignell@usgs.gov>
2020-01-23 14:17:33 -0500

    Update lifecycle_start_notebook.sh

--- lifecycle_start_notebook.sh
+++ lifecycle_start_notebook.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-cp /home/ec2-user/SageMaker/.condarc /home/ec2-user/.condarc
+cp /home/ec2-user/SageMaker/sagemaker-fargate-test/.condarc /home/ec2-user/.condarc
 cp -R /home/ec2-user/SageMaker/.aws /home/ec2-user
 chown -R ec2-user /home/ec2-user/.aws
